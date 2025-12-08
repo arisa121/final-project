98e8f302d93f3202d91c782f4ae382f45906c020
md sumon mia <engineercse19@gmail.com>
2025-12-08 10:57:32 +0600

    Routers SetUp

--- src/routers/Routers.jsx
+++ src/routers/Routers.jsx
@@ -4,8 +4,15 @@ import Home from "../pages/Home/Home/Home";
 import AuthLayout from "../LayOut/AuthLayout/AuthLayout";
 import Login from "../pages/Auth/Login/Login";
 import Register from "../pages/Auth/Register/Register";
+import PrivateRoute from "./PrivateRoute";
+import CitizenDashboard from "../dashboards/citizen/CitizenDashboard";
+import StaffRoute from "./StaffRoute";
+import StaffDashboard from "../dashboards/staff/StaffDashboard";
+import AdminRoute from "./AdminRoute";
+import AdminDashboard from "../dashboards/admin/AdminDashboard";
 
 export const router = createBrowserRouter([
+    // Public Routes
   {
     path: '/',
         Component: RootLayout,
@@ -14,7 +21,35 @@ export const router = createBrowserRouter([
                 index: true,
                 path: '/',
                 Component:Home
-            },    
+            },
+            // Citizen Dashboard(Private)
+            {
+                path: "/dashboard",
+                element: (
+                    <PrivateRoute>
+                        <CitizenDashboard></CitizenDashboard>
+                    </PrivateRoute>
+                )
+                
+            },
+            // Staff Dashboard 
+            {
+                path: "/staff-dashboard",
+                element: (
+                    <StaffRoute>
+                        <StaffDashboard></StaffDashboard>
+                    </StaffRoute>
+                )
+            },
+            // Admin Dashboard(Only Admin)
+            {
+                path: "/admin-dashboard",
+                element: (
+                    <AdminRoute>
+                        <AdminDashboard></AdminDashboard>
+                    </AdminRoute>
+                )
+            },
         ],
     },
     {
